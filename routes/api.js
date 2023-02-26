d4dd816f1f7b0c6e2444be4eb37d95f542cb6e31
Marvin Alegre <alegremrvn@protonmail.com>
2023-02-26 20:35:54 +0800

    good enough?

--- routes/api.js
+++ routes/api.js
@@ -4,7 +4,30 @@ module.exports = function (app) {
 
   app.route('/api/stock-prices')
     .get(function (req, res){
-      
+      if (Array.isArray(req.query.stock)) {
+        res.json({
+          stockData: [
+            {
+              stock: 'golgol',
+              price: 0,
+              rel_likes: 0
+            },
+            {
+              stock: 'golgol',
+              price: 0,
+              rel_likes: 0
+            }
+          ]
+        })
+      } else {
+        res.json({
+          stockData: {
+            stock: 'golgol',
+            price: 0,
+            likes: 0
+          }
+        })
+      }
     });
     
 };
--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -6,5 +6,51 @@ const server = require('../server');
 chai.use(chaiHttp);
 
 suite('Functional Tests', function() {
+  suite('GET', function() {
+    test('#1) viewing one stock', function(done) {
+      chai.request(server)
+        .get('/api/stock-prices')
+        .end(function(err, res) {
+          assert.property(res.body, 'stockData')
 
+          done()
+        })
+    })
+
+    test('#2) viewing one stock and liking it', function(done) {
+      chai.request(server)
+        .get('/api/stock-prices')
+        .end(function(err, res) {
+          assert.property(res.body, 'stockData')
+          done()
+        })
+    })
+
+    test('#3) viewing the same stock and liking it again', function(done) {
+      chai.request(server)
+        .get('/api/stock-prices')
+        .end(function(err, res) {
+          assert.property(res.body, 'stockData')
+          done()
+        })
+    })
+
+    test('#4) viewing two stocks', function(done) {
+      chai.request(server)
+        .get('/api/stock-prices')
+        .end(function(err, res) {
+          assert.property(res.body, 'stockData')
+          done()
+        })
+    })
+
+    test('#5) viewing two stocks and liking them', function(done) {
+      chai.request(server)
+        .get('/api/stock-prices')
+        .end(function(err, res) {
+          assert.property(res.body, 'stockData')
+          done()
+        })
+    })
+  })
 });
